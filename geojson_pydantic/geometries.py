f7b0e8c19cd86f8591620346822c68361e2248c4
Georg Arbesser <georg.arbesser-rastburg@tugraz.at>
2022-06-20 13:57:04 +0200

    fixed geometries.py

--- geojson_pydantic/geometries.py
+++ geojson_pydantic/geometries.py
@@ -13,7 +13,6 @@ from geojson_pydantic.types import (
     MultiLineStringCoords,
     MultiPointCoords,
     MultiPolygonCoords,
-    NumType,
     PolygonCoords,
     Position,
 )
@@ -177,7 +176,7 @@ class Polygon(_GeometryBase):
 
     @classmethod
     def from_bounds(
-        cls, xmin: NumType, ymin: NumType, xmax: NumType, ymax: NumType
+        cls, xmin: float, ymin: float, xmax: float, ymax: float
     ) -> "Polygon":
         """Create a Polygon geometry from a boundingbox."""
         return cls(
